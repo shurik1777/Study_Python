a2cf35a11108868d6b5712a76f1a7d76f754c1da
Aleksandr_Wosmerik <shurik1777@gmail.com>
2023-04-13 15:31:18 +0300

    add Task 12 work

---
+++ Task12.py
@@ -0,0 +1,29 @@
+"""
+Задача 12:
+Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница.
+Петя помогает Кате по математике. Он задумывает
+два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать.
+Для этого Петя делает две подсказки. Он называет
+сумму этих чисел S и их произведение P.
+Помогите Кате отгадать задуманные Петей числа.
+"""
+sum_of_numbers = int(input('Введите сумму задуманных чисел: '))
+product_of_numbers = int(input('Введите произведение суммы чисел: '))
+
+number_x = 1
+number_y = 1
+
+if product_of_numbers == 0:
+    number_x = 0
+    number_y = sum_of_numbers
+else:
+    for number_x in range(1, sum_of_numbers // 2 + 1):
+        number_y = sum_of_numbers - number_x
+        if number_x * number_y == product_of_numbers:
+            break
+            print(f'Первое загаданное число равно {int(number_x)}, а второе равно: {int(number_y)}')
+        else:
+            continue
+        break
+    else:
+        print("Данная комбинация не соответсвует")
