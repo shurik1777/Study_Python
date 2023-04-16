feda6501571c270d3fcbe3b490d4243bc25ce356
Alexandr_Wosmerik <shurik1777@gmail.com>
2023-04-16 10:30:08 +0300

    16 task done

--- SM3/Task18.py
+++ SM3/Task18.py
@@ -1,10 +1,10 @@
 import random
 
 number_arr = int(input("Введите количество элементов в массиве натуральным числом: "))
-items = [random.randint(1, number_arr*100) for _ in range(number_arr)]
+items = [random.randint(1, number_arr * 100) for _ in range(number_arr)]
 x_number = int(input("Задайте число к которому нужно найти ближайшее из массива: "))
 
-res = items[0] # допустим, ближайшее число к искомому первое в списке
+res = items[0]  # допустим, ближайшее число к искомому первое в списке
 for item in items:
     if abs(x_number - item) < abs(x_number - res):
         res = item
